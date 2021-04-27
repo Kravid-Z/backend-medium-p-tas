2e537fae187e570ca2744dec9f2cfe95a1b6594e
“Kravid-Z” <kravidz64@gmail.com>
2021-04-27 16:32:42 +0200

    routes added

---
+++ src/Components/articles/index.js
@@ -0,0 +1,77 @@
+import express from "express"
+
+import articlesModel from "./articles-schema.js"
+
+const articlesRouter = express.Router()
+
+articlesRouter.get("/", async (req, res, next) => {
+  try {
+    const articles = await articlesModel.find()
+    res.send(articles)
+  } catch (error) {
+    next(error)
+  }
+})
+
+articlesRouter.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const article = await articlesModel.findById(id)
+    if (article) {
+      res.send(article)
+    } else {
+      const error = new Error()
+      error.statusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+    next("While reading articles list a problem occurred!")
+  }
+})
+
+articlesRouter.post("/", async (req, res, next) => {
+  try {
+    const newArticle = new articlesModel(req.body)
+    const { _id } = await newArticle.save()
+
+    res.status(201).send(_id)
+  } catch (error) {
+    next(error)
+  }
+})
+
+articlesRouter.put("/:id", async (req, res, next) => {
+  try {
+    const article = await articlesModel.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    })
+    if (article) {
+      res.send(article)
+    } else {
+      const error = new Error(`Article with id ${req.params.id} not found`)
+      error.statusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+articlesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const article = await articlesModel.findByIdAndDelete(req.params.id)
+    if (article) {
+      res.send("Deleted")
+    } else {
+      const error = new Error(`Article with id ${req.params.id} not found`)
+      error.statusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default articlesRouter
