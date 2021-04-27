35ceb979372f808a5969dd75eccadb3133ea2241
“Kravid-Z” <kravidz64@gmail.com>
2021-04-27 16:35:04 +0200

    import errorHandlerGeneral in server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
-import articlesRouter from "./Components/articles/index.js"
+import articlesRouter from "./Components/articles/index.js";
+import { notFoundErrorHandler,
+    badRequestErrorHandler,
+    forbiddenErrorHandler,
+    catchAllErrorsHandler,} from "./Common/errorHandlerGeneral.js";
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
 
-server.use("/articles", articlesRouter)
+server.use("/articles", articlesRouter);
 
 
-console.log(listEndpoints(server))
+//GENERAL ERRORS
+server.use(notFoundErrorHandler);
+server.use(badRequestErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(catchAllErrorsHandler);
+
+
+
+console.log(listEndpoints(server));
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION, {
@@ -21,7 +33,7 @@ mongoose
   })
   .then(
     server.listen(port, () => {
-      console.log("Running on port", port)
+      console.log("Running on port", port);
     })
   )
-  .catch(err => console.log(err))
+  .catch((err) => console.log(err));
